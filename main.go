c2926e4267b50c799f54c5e9098441f9dfd94c4d
zt-winter <2996816019@qq.com>
2023-04-10 15:18:55 +0800

    优化GC设置，减少垃圾回收时间

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"os"
+	"runtime/debug"
+	"runtime/pprof"
 )
 
 func main() {
+	cpuf, err := os.OpenFile("cpu.pprof", os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer cpuf.Close()
+	pprof.StartCPUProfile(cpuf)
+	defer pprof.StopCPUProfile()
+
+	memoryf, err := os.OpenFile("memory.pprof", os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer memoryf.Close()
+	pprof.WriteHeapProfile(memoryf)
+
 	config := readConfig()
+	//setMemoryLimit中参数单位为字节
+	debug.SetMemoryLimit(15 << 30)
+	debug.SetGCPercent(-1)
 	extractFeature(config)
 }
